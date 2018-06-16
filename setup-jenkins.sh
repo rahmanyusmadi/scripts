c903060b2c9bc6d78bd5540fe9c4087437cab628
Abdul Rahman Yusmadi <31083956+yusmadi@users.noreply.github.com>
2018-06-16 10:15:56 +0800

    Update setup-jenkins.sh

--- setup-jenkins.sh
+++ setup-jenkins.sh
@@ -10,4 +10,3 @@ sudo yum remove java-1.7.0-openjdk -y
 sudo yum install java-1.8.0-openjdk jenkins git -y
 sudo service jenkins start
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword
